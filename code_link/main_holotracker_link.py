90eb3cced51b355f09f79785741df6417d19d33e
Simon BECKER <simbecker@gmail.com>
2026-06-05 10:34:09 +0200

    rename some files

--- code_link/main_holotracker_link.py
+++ code_link/main_holotracker_link.py
@@ -1,8 +1,29 @@
 """
-Minimal GUI for "code_link" app (renamed).
-Creates the left tabbed panel (PATH / ACTIONS) and a right-side 3D matplotlib axes
-matching the layout in the provided screenshots. This file only provides the GUI
-and stub callbacks. Run with: python main_holotracker_link.py
+Filename: main_holotracker_link.py
+
+Description:
+Main Graphical Application for executing the linkage of localized features into trajectories.
+Author: Simon BECKER
+mail: simon.becker@univ-lorraine.fr
+Date: 2026-06-01
+
+License:
+GNU General Public License v3.0
+
+Copyright (C) [2024] Simon BECKER
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
